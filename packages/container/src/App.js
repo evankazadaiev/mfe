4b7dcafb76dee3c3a382095ba323854942791b21
evankazadaiev <ivankazadaev21@gmail.com>
2023-08-23 00:13:11 +0300

    trigger github action 2.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>Hi There! It's a container app!</h1>
+            <h1>Hi There! It's a container app! Nice to see you here!</h1>
             <hr/>
             <MarketingApp />
         </div>
